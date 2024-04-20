1ba00b8458109957bf94e254d7d9c6caf524cc37
Youssef Abdelrahman <162054447+Enovac@users.noreply.github.com>
2024-04-20 10:35:09 +0200

    attempt to create div

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-test
-test2s
+function createFoodLog(){
+    const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("foodLog");
+  document.body.insertBefore(newDiv, currentDiv);
+}
