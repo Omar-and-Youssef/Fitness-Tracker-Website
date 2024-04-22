e87d0244a7ff8e4066e547f85ae34684c0b799a7
OmarMontaser1 <omarmontaser2005@gmail.com>
2024-04-22 18:01:10 +0200

    added comments, improved naming

--- main.js
+++ main.js
@@ -11,52 +11,59 @@ function updateFoodList(food){
  const foodList= document.querySelector(".foodList");
  const foodBox=foodBoxCreator(food);
  foodList.append(foodBox);
+   //add created food Box div to our list
 }
 function foodBoxCreator(food){
   const foodBox=document.createElement("div");
   foodBox.classList.add("foodBox");
-  foodBox.id=("foodBoxNo."+numberOfFoodBoxes++);
+  foodBox.id=("foodBoxNo."+numberOfFoodBoxes++); 
+  //create an id for each div
   const headingOfFoodBox=document.createElement("div");// HEADING DIV
   headingOfFoodBox.classList.add("headings");
   var headingNames=["Calories","Protein","Fats","Carbs"];
   for(var i=0;i<headingNames.length;i++){
     const theDiv=document.createElement("div");
-    theDiv.classList.add("ListTitle");
+    // theDiv.classList.add("ListTitle"); 
     theDiv.innerHTML=headingNames[i];
     headingOfFoodBox.append(theDiv);
-  }
+  }//add label names to headings box
   foodBox.append(headingOfFoodBox);
+
   const foodListContent=document.createElement("div");
   foodListContent.classList.add("foodListContent");//foodListContent Div
+
   var inputFoodValues=[food.calories.value,food.protein.value,food.fats.value,food.carbs.value];
   const foodName=document.createElement("div");
   foodName.classList.add("ListName");
-  foodName.innerHTML=food.name.value;
-  foodListContent.append(foodName);
+  foodName.innerHTML=food.name.value;//add name and values from log
+
+  foodListContent.append(foodName);//add names to content
+
   for(var i=0;i<inputFoodValues.length;i++){
     var theDiv=document.createElement("div");
     theDiv.classList.add("Value");
     theDiv.classList.add(foodListContent[i]);
     theDiv.innerHTML=inputFoodValues[i];
-    foodListContent.append(theDiv);
+    foodListContent.append(theDiv); //add values to content
   }
-  foodBox.append(foodListContent);
+
+  foodBox.append(foodListContent);//add content to box
   return foodBox;
 }
 
 
-var displaylogButton = document.querySelector(".display.foodL");
+var displaylogButton = document.querySelector(".foodLogButtons.display");
 displaylogButton.addEventListener("click", function() {
   document.querySelector(".foodLog").style.display="block";
 });
-var closeLogButton =document.getElementsByClassName("close")[0];
+var closeLogButton =document.querySelector(".foodLogButtons.close");
 closeLogButton.addEventListener("click", function() {
   document.querySelector(".foodLog").style.display="none";
 });
 
-var addFoodButton =document.querySelector(".add.food");
-  addFoodButton.addEventListener("click", function(){
-  var getInput=document.getElementsByClassName("test");
+var addFoodButton =document.querySelector(".foodLogButtons.add");
+  addFoodButton.addEventListener("click", function(){//add food item button
+  var getInput=document.getElementsByClassName("foodInput");
 
   for(var i=0;i<getInput.length;i++)
     getInput[i]=getInput[i].value;
@@ -64,7 +71,8 @@ var addFoodButton =document.querySelector(".add.food");
   document.querySelector(".foodLog").style.display="none";
 
   var myfood=new Food(getInput[0],getInput[1],getInput[2],getInput[3],getInput[4]);
-  console.log(myfood.name.value+" "+myfood.calories.value+" "+myfood.carbs.value+" "+myfood.protein.value+" "+myfood.fats.value);
+  console.log(myfood.name.value+" "+myfood.calories.value+" "+myfood.carbs.value+" "
+  +myfood.protein.value+" "+myfood.fats.value);//console test
 
   updateFoodList(myfood);
 })
